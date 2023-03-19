39ecd5b37e0a8b33bdb817b6a51d0f870e22b6cf
Daniel Khashev (Angelhe1m) <116936326+angelhe1m@users.noreply.github.com>
2023-03-20 00:26:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,20 @@
 // Copyright 2021 NNTU-CS
 int cbinsearch(int *arr, int size, int value) {
-  int lev = 0;
-  int prav = size - 1;
-  int kol = 0;
-  while (lev <= prav) {
-    int mid = (lev + prav) / 2;
+  int ll = 0;
+  int rg = size - 1;
+  int kl = 0;
+  while (ll <= rg) {
+    int mid = (ll + rg) / 2;
     if (value < arr[mid]) {
-      prav = mid - 1;
+      rg = mid - 1;
     } else if (value > arr[mid]) {
-      lev = mid + 1;
+      ll = mid + 1;
     } else {
       for (int i = 1; arr[mid] == arr[mid - i]; i++)
-        kol++;
+        kl++;
       for (int i = 0; arr[mid] == arr[mid + i]; i++)
-        kol++;
-      return kol;
+        kl++;
+      return kl;
     }
   }
   return 0;
