73dc712d7f610d3081ab4543b309658d85a7939f
Daniel Khashev (Angelhe1m) <116936326+angelhe1m@users.noreply.github.com>
2023-03-20 00:15:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,22 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int lev = 0;
+  int prav = size - 1;
+  int kol = 0;
+  while (lev <= prav) {
+    int mid = (lev + prav) / 2;
+    if (value < arr[mid]) {
+      prav = mid - 1;
+    } else if (value > arr[mid]) {
+      lev = mid + 1;
+    } else {
+      for (int i = 1; arr[mid] == arr[mid - i]; i++)
+        kol++;
+      for (int i = 0; arr[mid] == arr[mid + i]; i++)
+        kol++;
+      return kol;
+    }
+  }
+  return 0; 
 }
